8b0556a1d4fe134e294eadd4766c9878333d1dcc
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2023-08-19 09:03:52 +0000

    datatypes, stakc heap and strings

---
+++ day_02/datatypes_summary.js
@@ -0,0 +1,9 @@
+//Primitive data types
+//String, Number, NULL, undefined, Boolean, BigInt, Symbol --- Primitive types or Non Reference Types
+// Arrays, Functions and Objects --- Non Primitive Types or Reference Types
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id == anotherId);
+console.log(id);
---
+++ day_02/stack_heap_memories.js
@@ -0,0 +1,14 @@
+let name1 = "MonkeyDLuffy";
+let anotherName = name1;
+anotherName = "Roronoa Zoro";
+
+console.log(name1);
+console.log(anotherName);
+
+let myObj = {
+    "name" : "MonkeyDLuffy"
+}
+myObj2 = myObj;
+myObj2.name = "Zoro";
+console.log(myObj.name);
+console.log(myObj2.name);
