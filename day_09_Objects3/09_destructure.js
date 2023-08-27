f02486dbd9d44124fa0d6a38b93291d0603acf15
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2023-08-27 12:45:01 +0000

    destructuring of the objects keys values entries

---
+++ day_09_Objects3/09_destructure.js
@@ -0,0 +1,9 @@
+const userNames = {
+    user: "Balaji19970912",
+    email: "yoyo@balaji.com"
+}
+
+const {user} = userNames
+const {user:Balaji} = userNames
+console.log(user)
+console.log(Balaji)
