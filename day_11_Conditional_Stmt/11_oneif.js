15f25cf45ab72d4a751baf78674e31a23732b533
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2023-09-06 13:35:57 +0000

    condition if truthy and falsy

---
+++ day_11_Conditional_Stmt/11_oneif.js
@@ -0,0 +1,12 @@
+if(2 == "2") {
+    console.log("Why Executed?")
+}
+if(2 === "2") {
+    console.log("Executed")
+}
+
+if(1) {
+    console.log("Executed")
+} else {
+    console.log("Not executed")
+}
