6a9d21b693d15026003d3326a200fdd69002abae
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2023-09-10 18:06:07 +0000

    dom manipulations

---
+++ day_13_DOM/13_dom2.js
@@ -0,0 +1,20 @@
+const parent = document.querySelector('.parent')
+
+// console.log(parent)
+// console.log(parent.children)
+// console.log(parent.children[0])
+// console.log(parent.children[0].innerHTML)
+
+// for(let i = 0; i < parent.children.length; i++) {
+//     console.log(parent.children[i].innerHTML)
+// }
+
+// console.log(parent.firstElementChild)
+// console.log(parent.lastElementChild)
+
+const dayOne = document.querySelector('.day')
+// console.log(dayOne)
+
+// console.log(dayOne.parentElement)
+
+console.log("NODES: ",parent.childNodes)
